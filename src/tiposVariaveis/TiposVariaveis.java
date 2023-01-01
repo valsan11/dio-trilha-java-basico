c420338a7a25bbe5bad535205cfba5a0ae5e074d
origin <vpsantos11@gmail.com>
2022-12-31 22:44:19 -0300

    segunda alteração

--- src/tiposVariaveis/TiposVariaveis.java
+++ src/tiposVariaveis/TiposVariaveis.java
@@ -4,6 +4,12 @@ public class TiposVariaveis {
 
 	  public static void main(String[] args) {
 		  
-		  System.out.println("Hello, World!");
+		  short numeroCurto = 1;
+		  int numeroNormal = numeroCurto;
+		  short numeroCurto2 = (short)numeroNormal;
+		  
+		  final  int NUMERO = 1;
+		  //NUMERO =2;
+		  System.out.println("Hello, World!" + NUMERO);
 	  }
 }
